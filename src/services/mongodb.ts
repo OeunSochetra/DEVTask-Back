02ecc0e72f99bca8d4bf45e3acf78fc932351b08
OeunSochetra <oeunsochetra@gmail.com>
2024-12-03 15:03:56 +0700

    update connection

--- src/services/mongodb.ts
+++ src/services/mongodb.ts
@@ -1,14 +1,22 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
-const uri = process.env.MONGO_URI as string;
+
+let isConnected = false;
 
 export const connectDB = async () => {
+  if (isConnected) return;
+
   try {
+    const uri = process.env.MONGO_URI;
+    if (!uri)
+      throw new Error("MONGO_URI is not defined in environment variables.");
+
     await mongoose.connect(uri);
-    console.log(`MongoDB Connected: ${uri}`);
+    isConnected = true;
+    console.log("MongoDB connected successfully");
   } catch (error) {
-    console.error("Failed to connect to MongoDB:", error);
-    process.exit(1); // Exit process if connection fails
+    console.error("MongoDB connection error:", error);
+    throw error;
   }
 };
