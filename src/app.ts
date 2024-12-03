25ea9533dc565022930c560090e87d4441507975
OeunSochetra <oeunsochetra@gmail.com>
2024-12-03 13:39:19 +0700

    update app

--- src/app.ts
+++ src/app.ts
@@ -22,8 +22,6 @@ app.use("/api", BoardRoute);
 app.use("/api", mediaRoute);
 app.use("/api", privacyPolicyRoute);
 
-app.get("/", (req: any, res: any) => res.send("Express on Vercel"));
-
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
