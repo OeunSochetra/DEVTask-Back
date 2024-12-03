059d40bbd1be2fc8f96450ba0b700729f988e518
OeunSochetra <oeunsochetra@gmail.com>
2024-12-03 10:51:39 +0700

    .

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,26 @@
 import express from "express";
+import { connectDB } from "../src/services/mongodb";
+import featureJobRoute from "../src/routes/featureJobRoute";
+import spotlightRoute from "../src/routes/spotlightRoute";
+import programLangRoute from "../src/routes/programLangRoute";
+import BoardRoute from "../src/routes/boardRoute";
+import mediaRoute from "../src/routes/mediaRoute";
+import privacyPolicyRoute from "../src/routes/privacyPolicyRoute";
+import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
+connectDB();
 const app = express();
 app.use(express.json());
+app.use(cors()); // Use CORS middleware
+
+app.use("/api", featureJobRoute);
+app.use("/api", spotlightRoute);
+app.use("/api", programLangRoute);
+app.use("/api", BoardRoute);
+app.use("/api", mediaRoute);
+app.use("/api", privacyPolicyRoute);
 
 app.get("/", (req: any, res: any) => res.send("Express on Vercel"));
 
